72680692d4f7ef63b7d4079d35ac36d1c6c0bb8f
Bdaboy <167365862+Bdaboy@users.noreply.github.com>
2024-04-19 11:23:26 -0400

    Update relations.js testing manual update

--- relations.js
+++ relations.js
@@ -1,4 +1,10 @@
-var brothers = [];
+var brothers = [
+{
+    "name": "Jack W.",
+    "graduated": true,
+    "familystarted": "Saints"
+  },
+];
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = brothers;
 }
